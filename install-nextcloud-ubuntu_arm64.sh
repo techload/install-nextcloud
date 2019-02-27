397a91555254184f8c56a1f43a4ef7af3c81abcd
Carsten Rieger <47498887+criegerde@users.noreply.github.com>
2019-02-27 08:56:29 +0100

    ssl.conf
    changed order in ssl.conf
    removed old entries regarding nginx

--- install-nextcloud-ubuntu_arm64.sh
+++ install-nextcloud-ubuntu_arm64.sh
@@ -3,10 +3,10 @@
 # https://www.c-rieger.de
 # https://github.com/riegercloud
 # INSTALL-NEXTCLOUD.SH
-# Version 7.1 (ARM64)
+# Version 7.2 (ARM64)
 # Nextcloud 15
-# OpenSSL 1.1.1, TLSv1.3, NGINX 1.15.8, PHP7.3
-# January, 30th 2019
+# OpenSSL 1.1.1, TLSv1.3, NGINX 1.15.9, PHP7.3
+# February, 27th 2019
 ################################################
 # Ubuntu 18.04 LTS ARM64 - Nextcloud 15
 ################################################
@@ -449,11 +449,11 @@ ssl_session_cache shared:SSL:50m;
 ssl_session_tickets off;
 ssl_protocols TLSv1.3 TLSv1.2;
 ssl_ciphers 'TLS-CHACHA20-POLY1305-SHA256:TLS-AES-256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA512:DHE-RSA-AES256-GCM-SHA512:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384';
-ssl_ecdh_curve secp521r1:secp384r1:prime256v1;
+ssl_ecdh_curve secp521r1:secp384r1;
 ssl_prefer_server_ciphers on;
 ssl_stapling on;
 ssl_stapling_verify on;
-ssl_early_data on;
+#ssl_early_data on;
 EOF
 touch /etc/nginx/proxy.conf
 cat <<EOF >/etc/nginx/proxy.conf

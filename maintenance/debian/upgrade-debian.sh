9e78871ae1c168212bc81a222cafddbfec5c6779
Carsten Rieger <47498887+criegerde@users.noreply.github.com>
2019-02-27 09:22:13 +0100

    update all apps
    su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ app:update --all'

--- maintenance/debian/upgrade-debian.sh
+++ maintenance/debian/upgrade-debian.sh
@@ -11,6 +11,7 @@ sed -i "s/post_max_size=.*/post_max_size=10240M/" /var/www/nextcloud/.user.ini
 sed -i "s/output_buffering=.*/output_buffering='Off'/" /var/www/nextcloud/.user.ini
 chown -R www-data:www-data /var/www/nextcloud
 su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ update:check'
+su - www-data -s /bin/bash -c 'php /var/www/nextcloud/occ app:update --all'
 /usr/sbin/service php7.3-fpm restart
 /usr/sbin/service nginx restart
 exit 0

519809e1fc6aeaad682020171736713dbb818356
Carsten Rieger <47498887+criegerde@users.noreply.github.com>
2019-03-13 23:46:42 +0100

    Cosmetical Changes

--- maintenance/ubuntu/upgrade-ubuntu.sh
+++ maintenance/ubuntu/upgrade-ubuntu.sh
@@ -9,7 +9,7 @@ sudo -u www-data php /var/www/nextcloud/occ db:convert-filecache-bigint
 sudo -u www-data sed -i "s/upload_max_filesize=.*/upload_max_filesize=10240M/" /var/www/nextcloud/.user.ini
 sudo -u www-data sed -i "s/post_max_size=.*/post_max_size=10240M/" /var/www/nextcloud/.user.ini
 sudo -u www-data sed -i "s/output_buffering=.*/output_buffering='Off'/" /var/www/nextcloud/.user.ini
-sudo -u www-data php /var/www/nextcloud/occ update:check
+sudo -u www-data php /var/www/nextcloud/occ app:update --all
 /usr/sbin/service php7.3-fpm restart
 /usr/sbin/service nginx restart
 exit 0
